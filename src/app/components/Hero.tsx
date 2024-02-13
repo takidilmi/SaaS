1fbc09e051a77846e59e01b88b5953e46b047c01
taki dilmi <angeldilmi@gmail.com>
2024-02-14 00:45:31 +0100

    initialize app and basic structure

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const page = () => {
+const Hero = () => {
   return (
     <>
       <div></div>
@@ -8,4 +8,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Hero;
