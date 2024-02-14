cb8edc60d363414f9b562d96e6aacb71b0d9377c
taki dilmi <angeldilmi@gmail.com>
2024-02-14 19:13:39 +0100

    add salescard component

---
+++ src/app/components/Dashboard/SalesCard.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Image from 'next/image';
+
+type SalesProps = {
+  name: string;
+  email: string;
+  saleAmount: number;
+};
+
+export default function SalesCard(props: SalesProps) {
+  return (
+    <>
+      <div className="flex flex-wrap justify-between gap-3">
+        <section className="flex justify-between gap-3">
+          <div className="h-12 w-12 rounded-full bg-gray-100 p-1">
+            <Image
+              src={`https://api.dicebear.com/7.x/micah/svg?seed=${props.name}&backgroundType=gradientLinear&backgroundColor=b6e3f4,c0aede,d1d4f9,ffd5dc,ffdfbf`}
+              alt="avatar"
+              width={200}
+              height={200}
+            />
+          </div>
+          <div className="text-sm">
+            <p>{props.name}</p>
+            <div className="text-ellipsis overflow-hidden whitespace-nowrap w-[120px] sm:w-auto text-gray-400">
+              {props.email}
+            </div>
+          </div>
+          <p>{props.saleAmount}</p>
+        </section>
+      </div>
+    </>
+  );
+}
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -32,7 +32,7 @@ const cardData: CardProps[] = [
 const page = () => {
   return (
     <>
-      <div className='bg-white p-8'>
+      <div className="bg-white p-8">
         <section className="grid w-full grid-cols-1 gap-4 gap-x-8 transition-all sm:grid-cols-2 xl:grid-cols-4">
           {cardData.map((d, i) => (
             <Card
@@ -46,8 +46,16 @@ const page = () => {
         </section>
         <section className="grid grid-cols-1 gap-4 transition-all lg:grid-cols-2">
           <CardContent>
-            <p className='p-4 font-semibold'>Overview</p>
+            <p className="p-4 font-semibold">Overview</p>
             <BarChart />
+          </CardContent>
+          <CardContent>
+            <section>
+              <p>Recent Sales</p>
+              <p className="text-sm text-gray-400">
+                You made 49 more sales this month
+              </p>
+            </section>
           </CardContent>
         </section>
       </div>
