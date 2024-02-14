c0ee28c6547da1607c0074e574136f1f8ae13e4e
taki dilmi <angeldilmi@gmail.com>
2024-02-14 01:37:44 +0100

    add basic navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,10 +1,43 @@
 import React from 'react';
+import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <>
-      <div></div>
-    </>
+    <nav className="flex justify-end w-full ">
+      <a
+        className="self-center"
+        href="/"
+      >
+        Logo
+      </a>
+      <div className="flex justify-between items-center w-1/2 p-1 gap-2 px-2 backdrop-blur-md rounded-xl">
+        <div className="flex space-x-4">
+          <div className="bg-red-600 h-[2px] w-[120px] self-center"></div>
+          <a href="">Product</a>
+          <div className="dropdown">
+            <span>Learn</span>
+            <div className="dropdown-content">
+              <a href="">Learn 1</a>
+              <a href="">Learn 2</a>
+              <a href="">Learn 3</a>
+            </div>
+          </div>
+          <div className="dropdown">
+            <span>Solutions</span>
+            <div className="dropdown-content">
+              <a href="">Solution 1</a>
+              <a href="">Solution 2</a>
+              <a href="">Solution 3</a>
+            </div>
+          </div>
+          <a href="">Pricing</a>
+        </div>
+        <div className="flex space-x-3 flex-wrap items-center">
+          <a href="">Contact</a>
+          <a href="">SignIn</a>
+        </div>
+      </div>
+    </nav>
   );
 };
 
